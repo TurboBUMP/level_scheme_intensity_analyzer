9032102742838a4d286820f6bfad149340ca8ee2
Massimiliano Luciani <massimilianoluciani222@gmail.com>
2025-10-23 11:57:16 +0200

    1- Added some new spectra 2- Now the images are not displayed anymore insted, they are saved as png inside the relative spectra directory.

--- sauron.py
+++ sauron.py
@@ -29,19 +29,18 @@ lvlScheme.reset_index()
 def Energy(x, m, q, mean, sigma , amplitude):
     return np.asarray(amplitude * np.exp(-(x-mean)**2/(2*sigma**2)) + m*x + q )#+ c1*np.exp(c2*(x-mean))*(1-(np.exp(c3*(x-mean)**2)/(2*sigma**2))))
     
-def FitGauss(hist, q, mean, sigma, amplitude, window=6, plot_title=""):
+def FitGauss(hist, q, mean, sigma, amplitude, window=6, plot_title="", fig_dir=""):
 
-#    fig, ax = plt.subplots(1,1,figsize=(7,3))
-#    ax.bar(hist[int(mean-window):int(mean+window),0], hist[int(mean-window):int(mean+window),1])
+    fig, ax = plt.subplots(1,1,figsize=(7,3))
+    ax.bar(hist[int(mean-window):int(mean+window),0], hist[int(mean-window):int(mean+window),1])
     
     try:
         
         parameters, _ = curve_fit(Energy, hist[int(mean-window):int(mean+window),0], hist[int(mean-window):int(mean+window),1], p0=[-0.1,q,mean,sigma,amplitude])
-#        appo = np.linspace(int(mean-window),int(mean+window),500)
-#        ax.plot(appo, Energy(appo, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]),color="darkorange")
-#        ax.set_title(plot_title)
+        appo = np.linspace(int(mean-window),int(mean+window),500)
+        ax.plot(appo, Energy(appo, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]),color="darkorange")
         I = int(quad(Energy,int(mean-window),int(mean+window),args=(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]))[0]-np.sum(hist[int(mean-window):int(mean+window),1]))
-        print("I: ", I)
+        #print("I: ", I)
         #print("parameters: ", parameters)
 
     except: 
@@ -50,10 +49,13 @@ def FitGauss(hist, q, mean, sigma, amplitude, window=6, plot_title=""):
         #print("Error - curve_fit failed")
         #print("I: ", -np.sum(hist[int(mean-window):int(mean+window),1]))
         I = int(-np.sum(hist[int(mean-window):int(mean+window),1]))
-        print("I: ", I)
+        #print("I: ", I)
         parameters = [0,0,0,0,0]
 
 #    plt.show()
+    ax.set_title(plot_title)
+    plt.savefig(fig_dir + plot_title.replace(" ","-") + '.png', dpi=300)
+    plt.close()
     return parameters, I
 
 
@@ -79,5 +81,5 @@ with open(spectra_directory + args.pos_arg + '.' + 'out.txt', 'w') as f:
             
             for index, gammaray in subsetLevelScheme.iterrows():
                 
-                rFit, I = FitGauss(h, h[0][1] ,gammaray['Egamma-LITERATURE'], 2, h[int(gammaray['Egamma-LITERATURE'])][1], window=6, plot_title=file.replace(".dat","") + " " + str(gammaray['Egamma-LITERATURE']))
+                rFit, I = FitGauss(h, h[0][1] ,gammaray['Egamma-LITERATURE'], 2, h[int(gammaray['Egamma-LITERATURE'])][1], window=6, plot_title=file.replace(".dat","") + " " + str(gammaray['Egamma-LITERATURE']), fig_dir=spectra_directory)
                 print(int(I), ",", gammaray['Egamma-LITERATURE'], ",", file.split("#")[1].split(".")[0], ",", rFit[0],",",rFit[1],",",rFit[2],",",rFit[3],",",rFit[4],file=f)
