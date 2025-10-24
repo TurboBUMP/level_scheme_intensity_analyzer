d7242140ac042cdff83dd04d64d2686bb04609d0
Massimiliano Luciani <massimilianoluciani222@gmail.com>
2025-10-24 15:48:29 +0200

    Added last spectra --> Next step will be to run the main program over every histogram in the spectra directory-tree. Also added a countSpectra.py script that check if every gamma ray reported in the excel file has a corresponding .dat file in the directory-tree.

---
+++ spectra/countSpectra.py
@@ -0,0 +1,28 @@
+#!/bin/python3.12
+
+# This python script counts how many .dat file are present in the spectra sub-directories and if, for 
+# any gamma-ray transition in the excel file, there is the corresponding .dat somewhere in one of 
+# the sub-directories
+# 
+
+import os
+from os.path import isdir, join
+import pandas as pd
+
+_lvl = pd.read_excel("../../44Ca_ILL/intensities44CaCompressed.ods", sheet_name=0, usecols=[0,4,6])
+_lvl.reset_index()
+
+dirs = [dir for dir in os.listdir() if isdir(dir)]
+
+for energy in _lvl['Egamma-LITERATURE']:
+    energyIsPresent = False
+    for dir in dirs:
+        for file in os.listdir(join(os.getcwd(),dir)):
+            if file.endswith('.dat'):
+                if (float(energy) == float(file.replace('.dat',''))):
+                    energyIsPresent=True
+                else:
+                    pass
+
+    if energyIsPresent==False:
+        print(energy)
