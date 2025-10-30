6b0fa70b75ed50a9675f2104f260fd40ae1109ea
Massimiliano Luciani <massimilianoluciani222@gmail.com>
2025-10-30 15:53:31 +0100

    Sauron.py refined --> nothing important

--- sauron.py
+++ sauron.py
@@ -34,17 +34,34 @@ _stop_level_column = 6
 # 'gammaray_to_be_skipped' is a list of pairs that stores all the pairs of gate-and-spectra that (for some reasons)
 # need to be skipped.
 # Each pair should be inserted as (gammaray_energy, gate_energy)
-gammaray_to_be_skipped = [(263.53,887.5)]
+#gammaray_to_be_skipped = [(263.53,887.5)]
+gammaray_to_be_skipped = []
 
 import os
 from os.path import isdir,join
 import argparse
 
-parser = argparse.ArgumentParser(prog='SAURON', description='Search and Fit peaks program')
-parser.add_argument('path', type=str, help='path to the directory containing the spectra')
-parser.add_argument('-g','--gate', type=float, default=-1, help='energy of the gate that you want to use for the fit')
-parser.add_argument('-p', '--peak', type=float, default=-1, help='energy of the peak that you want to fit')
-parser.add_argument('--param', nargs=5, metavar=('m','q','mean','sigma','amplitude'), type=float, default=None, help='first guess for the fit parameters')
+parser = argparse.ArgumentParser(prog='SAURON',
+                                 description='Search and Fit peaks program')
+parser.add_argument('path', 
+                    type=str, 
+                    help='path to the directory containing the spectra')
+parser.add_argument('-g',
+                    '--gate', 
+                    type=float, 
+                    default=-1, 
+                    help='energy of the gate that you want to use for the fit')
+parser.add_argument('-p', 
+                    '--peak', 
+                    type=float, 
+                    default=-1, 
+                    help='energy of the peak that you want to fit')
+parser.add_argument('--param', 
+                    nargs=5, 
+                    metavar=('m','q','mean','sigma','amplitude'), 
+                    type=float, 
+                    default=None, 
+                    help='first guess for the fit parameters')
 args = parser.parse_args()
 
 import pandas as pd
@@ -59,7 +76,9 @@ from scipy.integrate import quad
 # Step 1
 # Load the excel file in a pandas dataframe using only the columns specified at the beggining of the file with 
 
-_lvlScheme = pd.read_excel("../44Ca_ILL/intensities44CaCompressed.ods",sheet_name=0,usecols=[_start_level_colum,_gamma_ray_energy_column,_stop_level_column])
+_lvlScheme = pd.read_excel("../44Ca_ILL/intensities44CaCompressed.ods",
+                           sheet_name=0,
+                           usecols=[_start_level_colum,_gamma_ray_energy_column,_stop_level_column])
 _lvlScheme.reset_index()
 
 # Step 2
@@ -80,7 +99,7 @@ def Gauss(x, mean, sigma, amplitude):
 def GaussPol1(x, m, q, mean, sigma , amplitude):
     return np.asarray(amplitude * np.exp(-(x-mean)**2/(2*sigma**2)) + m*x + q )#+ c1*np.exp(c2*(x-mean))*(1-(np.exp(c3*(x-mean)**2)/(2*sigma**2))))
     
-def FitGauss(hist, q, mean, sigma, amplitude, window=6, plot_title="", fig_dir="", par=[]):
+def FitGauss(hist, q, mean, sigma, amplitude, window=6, plot_title="", fig_dir="", par=[], savefig_flag=False):
     if(par==None):
         par=[-0.1,q,mean,sigma,amplitude]
 
@@ -89,13 +108,18 @@ def FitGauss(hist, q, mean, sigma, amplitude, window=6, plot_title="", fig_dir="
     
     try:
         
-        parameters, _ = curve_fit(GaussPol1, hist[int(mean-window):int(mean+window),0], hist[int(mean-window):int(mean+window),1], p0=par)
+        parameters, _ = curve_fit(GaussPol1, 
+                                  hist[int(mean-window):int(mean+window),0], 
+                                  hist[int(mean-window):int(mean+window),1], 
+                                  p0=par)
         appo = np.linspace(int(mean-window),int(mean+window),500)
-        ax.plot(appo, GaussPol1(appo, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]),color="darkorange")
+        ax.plot(appo, 
+                GaussPol1(appo, parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]),
+                color="darkorange")
         I_diff = int(quad(GaussPol1,int(mean-window),int(mean+window),args=(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]))[0]-np.sum(hist[int(mean-window):int(mean+window),1]))
-        I = quad(Gauss,int(mean-window),int(mean+window),args=(parameters[2], parameters[3], parameters[4]))[0]
-        #print("I: ", I)
-        #print("parameters: ", parameters)
+        I = quad(Gauss,
+                 int(mean-window),int(mean+window),
+                 args=(parameters[2], parameters[3], parameters[4]))[0]
 
     except: 
        
@@ -105,7 +129,14 @@ def FitGauss(hist, q, mean, sigma, amplitude, window=6, plot_title="", fig_dir="
 
     
     ax.set_title(plot_title)
-    plt.savefig(fig_dir + plot_title.replace(" ","-") + '.png', dpi=300)
+
+    if (savefig_flag==True):
+        plt.savefig(fig_dir + plot_title.replace(" ","-") + '.png', dpi=300)
+    if (savefig_flag==False):
+        if (input('Do you want to save the figure? (the fit params will not be automatically stored in the output.txt file\n')):
+            plt.savefig(fig_dir + plot_title.replace(" ","-") + 'png', dpi=300)
+        else:
+            pass
     
     if (args.peak != -1):
         plt.show()
@@ -139,9 +170,27 @@ if __name__ == '__main__':
         h = np.genfromtxt(filename)
         subsetLevelScheme = _lvlScheme[_lvlScheme['LevelLITERATURE'] == energyLevel]
         gammaray = args.peak
+
+        rFit, I_diff, I = FitGauss(h, 
+                                   h[0][1] ,
+                                   gammaray, 
+                                   2, 
+                                   h[int(gammaray)][1], 
+                                   window=6, 
+                                   plot_title=file.replace(".dat","") + " " + str(gammaray), 
+                                   fig_dir=spectra_directory, 
+                                   par=args.param)
         
-        rFit, I_diff, I = FitGauss(h, h[0][1] ,gammaray, 2, h[int(gammaray)][1], window=6, plot_title=file.replace(".dat","") + " " + str(gammaray), fig_dir=spectra_directory, par=args.param)
-        print(int(I_diff), ",", I, ",", gammaray, ",", file.replace('.dat',''), ",", rFit[0],",",rFit[1],",",rFit[2],",",rFit[3],",",rFit[4])
+        print("Integral Diff,Integral,TRANSITION,GATE,m,q,mean,sigma,amplitude",file=f)
+        print(int(I_diff), ",",
+              I, ",",
+              gammaray, ",",
+              file.replace('.dat',''), ",",
+              rFit[0],",",
+              rFit[1],",",
+              rFit[2],",",
+              rFit[3],",",
+              rFit[4])
         
     else:
 
@@ -171,10 +220,40 @@ if __name__ == '__main__':
                         for index, gammaray in subsetLevelScheme.iterrows():
         
                             if((gammaray['Egamma-LITERATURE'],float(file.replace('.dat',''))) in gammaray_to_be_skipped):
-                                print(1000000, ",", 0, ",", gammaray['Egamma-LITERATURE'], ",", file.replace('.dat',''), ",", 0,",",0,",",0,",",0,",",0,file=f)
+
+                                print(1000000, ",", 
+                                      0, ",", 
+                                      gammaray['Egamma-LITERATURE'], ",", 
+                                      file.replace('.dat',''), ",", 
+                                      0,",",
+                                      0,",",
+                                      0,",",
+                                      0,",",
+                                      0,
+                                      file=f)
+
                             else:
-                                rFit, I_diff, I = FitGauss(h, h[0][1] ,gammaray['Egamma-LITERATURE'], 2, h[int(gammaray['Egamma-LITERATURE'])][1], window=6, plot_title=file.replace(".dat","") + " " + str(gammaray['Egamma-LITERATURE']), fig_dir=spectra_directory)
-                                print(int(I_diff), ",", I, ",", gammaray['Egamma-LITERATURE'], ",", file.replace('.dat',''), ",", rFit[0],",",rFit[1],",",rFit[2],",",rFit[3],",",rFit[4],file=f)
+                                rFit, I_diff, I = FitGauss(h, 
+                                                           h[0][1], 
+                                                           gammaray['Egamma-LITERATURE'],
+                                                           2, 
+                                                           h[int(gammaray['Egamma-LITERATURE'])][1], 
+                                                           window=6, 
+                                                           plot_title=file.replace(".dat","") + " " + str(gammaray['Egamma-LITERATURE']), 
+                                                           fig_dir=spectra_directory, 
+                                                           savefig=True)
+                                print(int(I_diff),
+                                      ",",
+                                      I, 
+                                      ",",
+                                      gammaray['Egamma-LITERATURE'], ",",
+                                      file.replace('.dat',''), ",",
+                                      rFit[0],",",
+                                      rFit[1],",",
+                                      rFit[2],",",
+                                      rFit[3],",",
+                                      rFit[4],
+                                      file=f)
         
         else:
             pass
