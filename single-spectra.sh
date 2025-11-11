037ff69f29034c3f1048cb95884d246e8733d623
Massimiliano Luciani <massimilianoluciani222@gmail.com>
2025-11-11 17:20:51 +0100

    Going on with the checks. Last check made on gamma 556.8 with gate 1658.8 in directory 3913.8

--- single-spectra.sh
+++ single-spectra.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 ./sauron.py 1157.0208 -g 4932.8 -p 1157.004 --param -0.1 10 1157 2 350
+./sauron.py 2283.119 --gate 3731 --peak 1126.078 --param -0.1 10 1126 2 1000
 ./sauron.py 3357.29 -g 2037.9 -p 2200.1 --param -1 0 2200 2 100 -w 10
 ./sauron.py 3661.527 -g 1141.1 -p 2504.39 --param -1 0 2504 2 100 -w 4
 ./sauron.py 3661.527 -g 1839.7 -p 1777.973 --param -1 0 1778 2 100 -w 100
@@ -9,7 +10,14 @@
 ./sauron.py 3711.96 -g 1518.2 -p 2554.9 --param -1 0 2554 2 1000 -w 8
 ./sauron.py 3711.96 -g 1577.4 -p 667.3 --param -1 0 667 2 1000 -w 8
 ./sauron.py 3711.96 -g 202.1 -p 667.3 --param -1 0 667.3 1 1000 -w 15
+./sauron.py 3711.96 -g 202.1 -p 2554.9 --param -1 0 2554 1 1000 -w 4
 ./sauron.py 3711.96 -g 2063.2 -p 2554.9 --param -1 0 2554 1 50 --limit 6 4
 ./sauron.py 3711.96 -g 211.3 -p 667.3 --param -1 0 667 2 100 --limit 6 10
 ./sauron.py 3711.96 -g 211.3 -p 404.26 --param -1 0 404 2 100 --limit 6 10
+./sauron.py 3711.96 -g 1836.6 -p 667.3 --param -1 0 667 1 1000 --limit 10 7
+./sauron.py 3711.96 -g 1836.6 -p 2554.9 --param -1 0 2554 1 1000 --limit 7 8
+./sauron.py 3711.96 -g 7418.8 -p 667.3 --param -1 0 667 2 100 --limit 10 10
 ./sauron.py 3776.27 -g 1524.4 -p 1119.7 --param -1 0 1119 2 100 --limit 20 5
+./sauron.py 3676.092 -g 1420.2 -p 368.208 --param -1 0 368 1400 --limit 7 10
+./sauron.py 3776.27 -g 2896.7 -p 1119.7 --param -1 0 1119 2 100 --limit 20 5
+./sauron.py 3913.8 -g 1374.8 -p 869.47 --param -1 0 869 2 2000 --limit 10 5
